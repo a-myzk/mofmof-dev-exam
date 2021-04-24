6753e664aa549058b1260b80c2cfd5df1b33c4f2
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-04-24 16:08:12 +0900

    [Update]paramsメソッドで物件情報をを取得できるよう修正

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -64,6 +64,6 @@ class PropertiesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def property_params
-      params.fetch(:property, {})
+      params.require(:property).permit(:name, :price, :address, :age, :remark)
     end
 end
